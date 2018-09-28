2c71bef27b7ecee4be54a572e2eb09b6cf0b8ada
rotem.sade <rotem.sade@galacoral.com>
2018-09-28 16:06:08 +0300

    Initial Commit

---
+++ Hitchhiker Dev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hitchhiker Dev
+//
+//  Created by rotem.sade on 28/09/2018.
+//  Copyright © 2018 rotem.sade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
