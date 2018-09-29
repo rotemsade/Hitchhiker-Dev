07cfb5c6526c271ccde2441a879b15dccde1dcf1
rotem.sade <rotem.sade@galacoral.com>
2018-09-29 19:36:01 +0300

    Adding UIView fadeTo extension

---
+++ Hitchhiker Dev/Extensions/UIViewExt.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewExt.swift
+//  Hitchhiker Dev
+//
+//  Created by rotem.sade on 29/09/2018.
+//  Copyright © 2018 rotem.sade. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func fadeTo(alphaValue: CGFloat, withDuration duration: TimeInterval) {
+        UIView.animate(withDuration: duration) {
+            self.alpha = alphaValue
+        }
+    }
+}
--- Hitchhiker Dev/View/RoundedShadowButton.swift
+++ Hitchhiker Dev/View/RoundedShadowButton.swift
@@ -45,9 +45,7 @@ class RoundedShadowButton: UIButton {
                 if finished {
                     spinner.startAnimating()
                     spinner.center = CGPoint(x: self.frame.height / 2 + 1, y: self.frame.height / 2 + 1)
-                    UIView.animate(withDuration: 0.2, animations: {
-                        spinner.alpha = 1.0
-                    })
+                    spinner.fadeTo(alphaValue: 1.0, withDuration: 0.2)
                 }
             }
             self.isUserInteractionEnabled = false
